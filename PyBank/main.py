ace9d1bfd98fe21ce6b6f274d2f774c627776602
Julia <49284149+juliasweet@users.noreply.github.com>
2019-06-16 16:32:57 -0500

    Included new, corrected code and txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,157 +1,71 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 10 20:38:49 2019
 
-@author: julia
-"""
-import os
+#import modules
+import os 
 import csv
+#path
+csvpath = os.path.join('budget_data.csv')
 
-with open('budget_data.csv') as budget_data: #what to read
-    reader = csv.DictReader(budget_data, delimiter=',') #how to read
-    csvheader = next(csvreader) #ignore the header
-    dates=[] #creates a list
-    profitLoss=[] #creates a list
-    monthlyChange=[] #creates a list
-    # total=0 #was going to loop; used max instead
-    for row in csvreader:
-        dates.append(row[0]) #adds to the list
-        profitLoss.append(row[1]) #adds to the list
-        profitLoss = [int(row) for row in profitLoss]         
-            for row in (profitLoss):
-                monthlyChange = 1
-                monthlyChange = profitLoss[row+1] + profitLoss [row]
-                print(monthlyChange)
-                monthlyChange.append(row[1])
+#Variables; new lists
+dates=[]
+profitLoss=[]
+monthlyChange=[]
+
+#Open the file to read, skipping header    
+with open(csvpath, newline='') as csvfile:
+	csvreader = csv.reader(csvfile, delimiter=',')
+	csvheader = next(csvreader)
+#Fill lists
+	for row in csvreader:	
+		dates.append(row[0])
+		profitLoss.append(row[1])
+#Set as integer rather than string     
+profitLoss = [int(i) for i in profitLoss]         
+#Number of months
+len_profitLoss=len(profitLoss)
+#Net profit or loss for all months
+total_profitLoss=sum(profitLoss)
+
+#Calculate difference in profit or loss from month to month
+### Need to stop one month before end of range; no i+1 after last month.
+for i in range(len_profitLoss):
+    if i < len_profitLoss -1:
+        monthlyChange.append(profitLoss[i+1]-profitLoss[i])
         
-
-profitLoss = [int(i) for i in profitLoss] 
-
-######################
-profitLoss.index(max(profitLoss)) 
-profitLoss.index(min(profitLoss)) 
-
-dates.index 
+#To correctly calculate average, divide by length of monthlyChange list, not
+        #all rows (monthlyChange has one less row)
+changeMonths=(len(monthlyChange))
+maxIncrease=max(monthlyChange)
+maxDecrease=min(monthlyChange)
+totalChange=sum(monthlyChange)
 
 
+averageChange=(totalChange / changeMonths)
+
+##Assign index. Find corresponding index for month. 
+monthlyChange.index(max(monthlyChange)) 
+monthlyChange.index(min(monthlyChange)) 
+
+dates.index ##Don't forget to adjust to avoid off by one error below
+
+##Summary to display
 print(f"-----------------------------------------------------")
 print(f"Summary Data")
-print(f"Total months: {len(dates)}")
-print(f"Total: ${sum(profitLoss)}" )
-###Average goes below
-print(f"Average change: ${sum(profitLoss) / len(profitLoss)}")
-print(f"Greatest increase in profits: {dates[profitLoss.index(max(profitLoss))]}  (${max(profitLoss)})")
-print(f"Greatest decrease in profits: {dates[profitLoss.index(min(profitLoss))]}  (${min(profitLoss)})")
+print(f"Total Months: {len(dates)}")
+print(f"Total: ${(total_profitLoss)}" )
+print(f"Average change: ${(averageChange)}")
+print(f"Greatest increase in profits: {dates[monthlyChange.index(max(monthlyChange))+1]}  (${(maxIncrease)})")
+print(f"Greatest decrease in profits: {dates[monthlyChange.index(min(monthlyChange))+1]}  (${(maxDecrease)})")
 print(f"--------------------------------------------------------")
  
-
-##Create a dictionary
-#budgetData={}
-
-##How to populate dictionary with values from csv file? 
-#import os
-#import csv
-#budget_Data = csv.DictReader(open("budget_data.csv")
-#for str,int in budget_Data,budget_Data.values():
-    
-    
-##Import necessary modules
-#import os
-#import csv
-#import os.path
-
-#f=open(budget_data.csv)
-#csv_f=csv.reader(f)
-#profitLoss=[]
-#for row in csv_f:
- #   profitLoss.append(row[1])
-#f.close()
-#print (len(profitLoss))
-
-#Connect to file   
-#os.getcwd()
-#'budget_data.csv'
-
-# Count total_rows to determine number of months for which data exists. 
-#Figured out damned f string. 
-#csvpath=os.path.join('.\\budget_data.csv')
-#with open(csvpath, newline='') as csvfile:
- #   csvreader = csv.reader(csvfile, delimiter=',')
-  #  csv_header = next(csvfile)
-   # total_rows = sum(1 for row in csvfile)
-    #print (f"Months: {total_rows}")
-    
-#Trying to establish values based on location?
-#with open(csvpath, newline='') as csvfile:
- #   csvreader = csv.reader(csvfile, delimiter=',')
-  #  csv_header = next(csvfile)
-   # for row in csvfile:
-    #    months = row(0)
-     #   profitLoss = row(1)
-    
-#Can probably do this all at once. 
-    
-    
-#months = 0
-#net_profitLoss = 0
-
-
-#while profitLoss != 0: #as long as there is a value in 2nd index
- #   months += 1 #add to the months counter
-  #  net_profitLoss +=profitLoss
-    
-#avg_profitLoss = net_profitLoss/months
-
-#print(avg_profitLoss)
- 
-
-#Trying to turn into an array? 
-#dates = []
-#values = []
-
-#with open('budget_data.csv') as csvfile:
- #   csvreader = csv.reader(csvfile, delimiter=',')
-  #  for row in csvreader:
-   #     dates.append(row[0])
-    #    values.append(row[1])
-
-#print(dates)
-#print(values)
-
-#csvpath=os.path.join('.\\budget_data.csv')
-#def print_values(budget_data):
- #   month = str(budget_data[2])
-  #  value = int(budget_data[1])
-   # for number in numbers:
-    #    print(print_values)    
-
-#Testing path
-#csvpath=os.path.join('.\\budget_data.csv')
-#with open(csvpath, newline='') as csvfile:
-  #  csvreader = csv.reader(csvfile, delimiter=',')
-   # print(csvreader)
-    #csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
-    #for row in csvreader:
-     #   print(row)
-
-# Count total_rows to determine number of months for which data exists. 
-#Figured out damned f string. 
-#csvpath=os.path.join('.\\budget_data.csv')
-#with open(csvpath, newline='') as csvfile:
- #   csvreader = csv.reader(csvfile, delimiter=',')
-  #  csv_header = next(csv)
-   # total_rows = sum(1 for row in csvfile)
-    #print (f"Months: {total_rows}")
-           
-
-        
-# Find highestProfit.
-        
-# Find greatestLoss.
-        
-#Print financialAnalysis.
-        
-        
-#Export Text File of results.
-
+###Text file
+file = open("budget_data_Summary.txt", "w")
+file.write("Budget Summary Data\n")
+file.write(f"-----------------------------------------------------\n")
+file.write(f"Summary Data\n")
+file.write(f"Total Months: {len(dates)}\n")
+file.write(f"Total: ${(total_profitLoss)}\n" )
+file.write(f"Average change: ${(averageChange)}\n")
+file.write(f"Greatest increase in profits: {dates[monthlyChange.index(max(monthlyChange))+1]}  (${(maxIncrease)})\n")
+file.write(f"Greatest decrease in profits: {dates[monthlyChange.index(min(monthlyChange))+1]}  (${(maxDecrease)})\n")
+file.write(f"--------------------------------------------------------\n")
+file.close()
